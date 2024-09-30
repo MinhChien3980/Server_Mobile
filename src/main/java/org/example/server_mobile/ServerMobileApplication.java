f224eff06be3bb995fcd630a458805a3f551f844
MinhChien3980 <chien.bm3980@gmail.com>
2024-09-30 10:40:40 +0700

    Initial commit

---
+++ src/main/java/org/example/server_mobile/ServerMobileApplication.java
@@ -0,0 +1,13 @@
+package org.example.server_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerMobileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/server_mobile/ServerMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.server_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
