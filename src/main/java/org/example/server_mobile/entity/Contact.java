48c55f549f10289a7f44880dc5727263c9a7ff6f
NgocTaiPhan <ngoctaiphan.cv@gmail.com>
2025-01-09 13:50:07 +0700

    Add gender entity

---
+++ src/main/java/org/example/server_mobile/entity/Contact.java
@@ -0,0 +1,34 @@
+package org.example.server_mobile.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String description;// Noi dung feedback
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    User user;// id nguoi dung
+
+    @CreatedDate
+    @UpdateTimestamp
+    Date createdAt;// thoi gian tao feedback
+}
