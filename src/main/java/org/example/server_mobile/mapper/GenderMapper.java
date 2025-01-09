0c9358f395eae6d6293f83c8fd5a1770b9e285dd
NgocTaiPhan <ngoctaiphan.cv@gmail.com>
2025-01-10 00:27:31 +0700

    Remove age group and gender

--- src/main/java/org/example/server_mobile/mapper/GenderMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.server_mobile.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface GenderMapper {
-}
--- src/main/java/org/example/server_mobile/repository/GenderRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.server_mobile.repository;
-
-import org.example.server_mobile.entity.Gender;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenderRepo extends JpaRepository<Gender, Long> {
-}
